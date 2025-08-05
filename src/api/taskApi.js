17dd4b249433c571e32d17a04750d4ff85c82c29
Sahith21 <kesaboinasahith@gmail.com>
2025-08-05 22:25:20 +0530

    changed backend url

--- src/api/taskApi.js
+++ src/api/taskApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/tasks';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const getTasks = () => axios.get(BASE_URL);
 export const addTask = (task) => axios.post(BASE_URL, task);
